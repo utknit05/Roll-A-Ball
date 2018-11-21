5630c9f09baf6fd0d5108a0639fd8ab1ea890aea
Utkarsh <mac@Macs-MacBook-Pro.local>
2018-11-21 16:12:12 +0530

    Comming Roll-A-Ball Game's files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody rb;
+    private float count;
+    public Text score;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        score.text = count.ToString();
+    }
+
+    void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+
+        rb.AddForce(movement, ForceMode.VelocityChange);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("PickUp")){
+            //other.gameObject.SetActive(false);
+            Destroy(other.gameObject);
+            count++;
+            score.text = count.ToString();
+        }
+    }
+}
---
+++ Assets/Scripts/Rotater.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class Rotater : MonoBehaviour {
+
+	void Update () {
+        transform.Rotate(15 * Time.deltaTime, 30 * Time.deltaTime, 45 * Time.deltaTime);
+	}
+}
